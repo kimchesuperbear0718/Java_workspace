5c29b2b855bb4eaec12c0dbef2fe327dfe834795
parkmanbung <kimchesuperbear@gmail.com>
2026-08-24 11:00:59 +0900

    Add Java lessons through day0821

---
+++ app0821/src/animal/bird/Mybatis.java
@@ -0,0 +1,8 @@
+package animal.bird;
+
+public class Mybatis extends  Bird{
+
+	public void jack(){
+		System.out.println("Â±Â±´ë¿ä");
+	}
+}
---
+++ apple_review/src/use/UseApple.java
@@ -0,0 +1,16 @@
+package use;
+
+import fruit.Apple;
+
+public class UseApple {
+
+public static void main(String[] args) {
+
+Apple apple = new Apple("청사과", 500);
+
+System.out.println(apple.getName());
+
+System.out.println(apple.getPrice());
+
+	}
+}
